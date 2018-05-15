29a46913027dce145c15990b6e6bf4f626751f27
github username <github email address>
2018-05-15 10:40:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+class Driver {
+	constructor(name, date){
+		this.name = name
+		this.startDate = new Date(date);
+	}
 
+	yearsExperienceFromBeginningOf(endDate){
+		let startYear = this.startDate.getFullYear();
+		let years = endDate - startYear;
+		return years;
+	}
+}
+
+class Route{
+	constructor(beginningLocation, endingLocation){
+		this.beginningLocation = beginningLocation
+		this.endingLocation = endingLocation
+	}
+
+	blocksTravelled(){
+		let blocksTravelled = this.endingLocation.vertical -this.beginningLocation.vertical;
+		return blocksTravelled;
+	}
+
+	estimatedTime(){
+		let minutes = this.blocksTravelled() / 3;
+		return minutes;
+	}
+}
